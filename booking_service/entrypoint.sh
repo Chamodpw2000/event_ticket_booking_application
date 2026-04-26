1a964bcdfefb3aafb27412ee5dd6d2efbddb15a1
Tiran Wijerathne <tiranwijeratne@gmail.com>
2026-04-26 17:29:46 +0530

    Revert "Origin/tiran3"

--- booking_service/entrypoint.sh
+++ booking_service/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-echo "Running Prisma db push..."
-npx prisma db push --accept-data-loss
+echo "Running Prisma migrations..."
+npx prisma migrate deploy
 echo "Starting service..."
 exec node index.js
